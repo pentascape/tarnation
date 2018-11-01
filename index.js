cbbccdda46057d170ba7a339f28f0e7f9540d3a4
Alex Powell <alexjh.powell@gmail.com>
2018-11-01 17:34:07 +0000

    Testing repository functions.

--- index.js
+++ index.js
@@ -1,36 +1,123 @@
 'use strict';
-
+const { DynamoDB } = require('aws-sdk');
+const Converter = DynamoDB.Converter;
 
 
 class Repository {
-  constructor(table) {
+  constructor(props) {
+    this.validateProperties(props);
+    this.client = props.client;
+    this.table = props.table;
     this.versions = [];
-    this.table = table;
-    this.validateProperties();
   }
 
-  validateProperties() {
-    if ( typeof this.table !== 'string' || this.table === '' ) {
+  validateProperties(props) {
+    if ( typeof props.table !== 'string' || props.table === '' ) {
       throw new Error('Table should be a non-empty string value');
     }
   }
 
   addVersion(version) {
+    this.versions.push(version);
+  }
 
+  afterLoad(item) {
+    console.log(item);
+    if ( !item.hasOwnProperty('$schema') || typeof item.$schema !== 'string' || item.$schema === '' ) {
+      console.warn('No schema detected for object');
+    }
+
+    const currentVersionIndex = this.versions.findIndex(version => version.$id === item.$schema);
+    const newVersions = this.versions.slice(currentVersionIndex);
+    if ( newVersions.length <= 0 ) {
+      const proxyHandler = new Item();
+      return new Proxy(item, proxyHandler);
+    }
+
+    const proxyHandler = new Item({
+      migrations: newVersions
+    });
+
+    return new Proxy(item, proxyHandler);
+  }
+
+  getItem(params) {
+    return this.client
+     .getItem(params)
+      .promise()
+      .then(response => {
+        if ( !response.Item ) {
+          return null;
+        }
+
+        return this.afterLoad(Converter.unmarshall(response.Item))
+      })
+  }
+
+  putItem(params) {
+    this.migrate(params.Item);
+
+    return this.client
+      .putItem(params)
+      .promise()
+      .then(response => {
+      })
+  }
+
+  query(params) {
+    return this.client
+      .query(params)
+      .promise()
+      .then(response => {
+        return response.Items.map(item => this.migrate(item))
+      })
+  }
+
+  scan(params) {
+    return this.client
+      .scan(params)
+      .promise()
+      .then(response => {
+        return response.Items.map(item => this.migrate(item))
+      })
+  }
+}
+
+
+class Item {
+  constructor(props) {
+    if ( props === undefined ) {
+      this.migrations = [];
+    }
+  }
+
+  get(oTarget, sKey) {
+    return oTarget[sKey] || undefined;
+  }
+
+  apply(target, that, args) {
+    console.log('arguments', arguments);
+  }
+
+  hasMigrations() {
+    return this.migrations.length > 0;
+  }
+
+  migrate() {
+    if ( !this.hasMigrations() ) {
+      return;
+    }
   }
 }
 
 
 class Version {
-  constructor(schema, sequence) {
+  constructor(schema) {
     this.schema = schema;
-  }
-
-  migrate() {
-    return {};
   }
 }
 
 module.exports = {
   Repository: Repository,
+  Item: Item,
 };
--- test/repository.js
+++ test/repository.js
@@ -1,22 +1,75 @@
 'use strict';
 
-const faker = require('faker');
 const { assert, expect } = require('chai');
-const { Repository } = require('../index');
+const sinon = require('sinon');
+const faker = require('faker');
+const { DynamoDB } = require('aws-sdk');
+const { Repository, Item } = require('../index');
 
 
 describe('Repository', function () {
-  it('should accept a table name constructor argument', function () {
+  it('should accept an object constructor argument', function () {
+    const ddbClient = new DynamoDB();
     const tableName = faker.lorem.word();
-    const repository = new Repository(tableName);
+    const repository = new Repository({client: ddbClient, table: tableName});
 
     assert.equal(repository.table, tableName);
+    assert.equal(repository.client, ddbClient);
   });
 
   it('should throw an error when no table name is provided', function () {
     expect(function () {
-      const repository = new Repository();
-
+      const ddbClient = new DynamoDB();
+      new Repository({client: ddbClient});
     }).to.throw(Error);
+  });
+});
+
+
+describe('Repository.getItem', function () {
+  it('should raise a warning when a loaded item does not have a $schema property', function () {
+    const ddbClient = new DynamoDB();
+    const tableName = faker.lorem.word();
+    const repository = new Repository({client: ddbClient, table: tableName});
+
+    const consoleWarnSpy = sinon.spy(console, 'warn');
+    const getItemStub = sinon.stub(ddbClient, 'getItem');
+    getItemStub.returns({
+      promise: () => Promise.resolve({
+        Item: DynamoDB.Converter.marshall({
+          id: faker.random.uuid()
+        })
+      })
+    });
+
+    return repository.getItem({})
+      .catch(() => {})
+      .then(() => {
+        assert(consoleWarnSpy.called);
+        console.warn.restore();
+      });
+  });
+
+  it('should return an `Item` object for a retrieved item', function () {
+    const ddbClient = new DynamoDB();
+    const tableName = faker.lorem.word();
+    const repository = new Repository({client: ddbClient, table: tableName});
+
+    const getItemStub = sinon.stub(ddbClient, 'getItem');
+    getItemStub.returns({
+      promise: () => Promise.resolve({
+        Item: DynamoDB.Converter.marshall({
+          $schema: faker.system.commonFileName('json'),
+          id: faker.random.uuid()
+        })
+      })
+    });
+
+    return repository.getItem({})
+      .then(item => {
+        assert.instanceOf(item, Object);
+
+        // console.log(item.hasMigrations());
+      });
   });
 });
